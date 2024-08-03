55ed003f68a7f620d6d1726ee9d5c452e3e719ef
Sourav Banerjee <souravbnjee07@gmail.com>
2024-08-03 09:32:01 +0530

    Added boiler plate code
    Added necessary libraries and created the class

---
+++ src/algorithms/ga.py
@@ -0,0 +1,16 @@
+# necessary libraries
+import numpy as np
+from pymoo.problems.functional import FunctionalProblem
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.optimize import minimize
+
+
+class GeneticAlgorithm:
+    def __init__(self) -> None:
+        pass
+
+    def train(self) -> None:
+        pass
+
+    def predict(self) -> None:
+        pass
